e706803347796b03b81b8e2442e843d888404b72
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-02-08 14:16:44 -0800

    Create schedule.cpp

---
+++ schedule.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Course.h"
+
+int main()
+{
+     Course programming("CSCI", 211, 1000);
+     Course english("ENGL", 130, 1400);
+     Course physics("PHYS", 204, 800);    
+
+     programming.print();
+     english.print();
+     physics.print();
+}
